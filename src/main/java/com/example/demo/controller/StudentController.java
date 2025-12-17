e8b9a38250e2c7086bedc8b0995a4747873a6804
bala-amypo <bala@amypo.in>
2025-12-17 08:51:54 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -2,12 +2,13 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.StudentEntity;
 import com.example.demo.service.StudentService;
-
+import java.util.*;
 
 @RestController
 public class StudentController {
@@ -20,6 +21,8 @@ public class StudentController {
         return ser.addStudents(student);
     }
     @GetMapping("/getStudents")
-    public List<StudentEntity> get
+    public List<StudentEntity> getStudents(){
+        return ser.getStudents();
+    }
     
 }
--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,9 +1,11 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.StudentEntity;
+import java.util.*;
 
 public interface StudentService {
 
     public StudentEntity addStudents(StudentEntity student);
+    public List<StudentEntity> getStudent();
 
 }
