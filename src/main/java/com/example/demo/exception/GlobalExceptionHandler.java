2bc611deb0b9f7e87a4008218168f7326583fb40
bala-amypo <bala@amypo.in>
2025-12-23 03:52:28 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 
 
 public class GlobalExceptionHandler{
-    
+    @Exceptionhandler(ResourceNotFoundException.java)
+    public ResponseEntity<String>GlobalExceptionHandler (ResourceNotFoundException ex){
+        return ResponseEntity.status(404).body(ex.)
+    }
 }
